e2d214dd10661481991b58b538a5c362cb618841
SupornoChaudhury <85339011+SupornoChaudhury@users.noreply.github.com>
2023-11-16 02:41:51 +0530

    complete the prometheus code

--- internal/controllers/k8x_prometheus.go
+++ internal/controllers/k8x_prometheus.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/shukra-in-spirit/k8x/internal/constants"
@@ -48,6 +52,48 @@ func (prom *PrometheusInstance) GetPrometheusData(ctx context.Context, promQuery
 }
 
 func (prom *PrometheusInstance) GetPrometheusDataWithinRange(ctx context.Context, promQuery string, startTime time.Time, endTime time.Time, steps string) (*models.PrometheusDataSetResponse, error) {
-	responseDataFrame := models.PrometheusDataSetResponse{}
-	return &responseDataFrame, nil
+	// Create a JSON payload with the query_range parameters
+	queryRangeJSON := map[string]interface{}{
+		"query":  promQuery,
+		"start":  startTime,
+		"end":    endTime, // Set end time to the current time
+		"step":   steps,   // Step size for the range vector in seconds (e.g., 20s)
+		"format": "json",
+	}
+	queryRangeBytes, err := json.Marshal(queryRangeJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a POST request to the Prometheus query_range API
+	req, err := http.NewRequest("POST", prom.url, bytes.NewBuffer(queryRangeBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	// Set headers for the request
+	req.Header.Set("Content-Type", "application/json")
+
+	// Perform the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the JSON response into the QueryResult struct
+	var result models.PrometheusDataSetResponse
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
 }
--- internal/models/structs.go
+++ internal/models/structs.go
@@ -2,8 +2,12 @@ package models
 
 import "time"
 
-type PrometheusDataSetResponse struct {
+type PrometheusDataSetResponseItem struct {
 	Timestamp time.Time `json:"timestamp"`
 	CPU       float32   `json:"cpu"`
 	Memory    float32   `json:"memory"`
+}
+
+type PrometheusDataSetResponse struct {
+	PromItemList []PrometheusDataSetResponseItem `json:"items"`
 }
